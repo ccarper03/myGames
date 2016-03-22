6978f4928c310ff9dd1d66929208489bbea94b22
Charles Carper <ccarper03@gmail.com>
2016-03-22 17:06:18 -0400

    March 22

---
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuFunctions : MonoBehaviour 
+{
+	public void BtnPlayClick()
+	{
+		Application.LoadLevel("Quanternions");
+	}
+	public void BtnOptionsClick()
+	{
+		Application.LoadLevel("Menu_Options");
+	}
+	public void BtnCreditsClick()
+	{
+		Application.LoadLevel("GetButton and GetKey");
+	}
+	public void BtnExitClick()
+	{
+		Application.LoadLevel("MouseClick");
+	}
+}
