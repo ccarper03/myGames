b7769ea52f74f6d04a1acee58137b32e106bf531
Charles Carper <ccarper03@gmail.com>
2016-03-23 09:43:32 -0400

    Start Menu

--- Assets/Scripts/MainMenuFunctions.cs
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -9,14 +9,36 @@ public class MainMenuFunctions : MonoBehaviour
 	}
 	public void BtnOptionsClick()
 	{
-		Application.LoadLevel("Menu_Options");
+		gameObject.SetActive(false);
 	}
 	public void BtnCreditsClick()
 	{
 		Application.LoadLevel("GetButton and GetKey");
+
 	}
 	public void BtnExitClick()
 	{
-		Application.LoadLevel("MouseClick");
+		//Application.Quit ();
+		Debug.Log("You have Quit the program");
+	}
+	public void BTN_Ctr()
+	{
+		Debug.Log("Controls");
+	}
+	public void BTN_SFX()
+	{
+		Debug.Log("Sound Effects");
+	}
+	public void BTN_ScM()
+	{
+		Debug.Log("Social Media");
+	}
+	public void BTN_Bck()
+	{
+		Debug.Log("Back");
+	}
+	public void BTN_Msc()
+	{
+		Debug.Log("Music");
 	}
 }
